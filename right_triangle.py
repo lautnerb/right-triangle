a3904f28825cd43116b6c4bc1f348f5804a868a8
Balazs Lautner <lautner.balazs@gmail.com>
2019-06-30 18:02:45 +0200

    calculates other leg from a leg and hypotenuse

--- right_triangle.py
+++ right_triangle.py
@@ -1,3 +1,6 @@
+import math
+
+
 class RightTriangle:
     def __init__(
             self,
@@ -11,6 +14,16 @@ class RightTriangle:
         self._c = c
         self._a_angle = a_angle
         self._b_angle = b_angle
+
+        if self._has_leg_and_hypotenuse:
+            try:
+                self._init_from_leg_and_hypotenuse()
+            except ValueError:
+                raise ValueError(f"Inconsistent RightTriangle(\n"
+                                 + f"\ta = {a}"
+                                 + f"\tb = {b}"
+                                 + f"\tc = {c}"
+                                 + "a or b should not be given")
 
     @property
     def a(self) -> float:
@@ -31,3 +44,33 @@ class RightTriangle:
     @property
     def b_angle(self) -> float:
         return self._b_angle
+
+    def _has_leg_and_hypotenuse(self):
+        return (self._has_leg()
+                and self._has_hypotenuse())
+
+    def _has_leg(self):
+        return (self._a is not None
+                or self._b is not None)
+
+    def _has_hypotenuse(self):
+        return self._c is not None
+
+    def _init_from_leg_and_hypotenuse(self):
+        leg = self._a if self._a is not None else self._b
+        hypotenuse = self._c
+        other_leg = math.sqrt(hypotenuse ** 2 - leg ** 2)
+        self._set_other_leg(other_leg)
+
+    def _set_other_leg(self, other_leg):
+        if self._a is None:
+            self._a = other_leg
+        elif self._b is None:
+            self._b = other_leg
+        else:
+            self._raise_error_if_attribute_inconsistent(self._b, other_leg)
+
+    @staticmethod
+    def _raise_error_if_attribute_inconsistent(attribute_value, expected_value):
+        if attribute_value != expected_value:
+            raise ValueError()

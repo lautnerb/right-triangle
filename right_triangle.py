1db5ee4a3a9cad799850fb9931b87a0d94750b86
Balazs Lautner <lautner.balazs@gmail.com>
2019-06-30 17:10:15 +0200

    added properties

--- right_triangle.py
+++ right_triangle.py
@@ -11,3 +11,23 @@ class RightTriangle:
         self._c = c
         self._a_angle = a_angle
         self._b_angle = b_angle
+
+    @property
+    def a(self) -> float:
+        return self._a
+
+    @property
+    def b(self) -> float:
+        return self._b
+
+    @property
+    def c(self) -> float:
+        return self._c
+
+    @property
+    def a_angle(self) -> float:
+        return self._a_angle
+
+    @property
+    def b_angle(self) -> float:
+        return self._b_angle

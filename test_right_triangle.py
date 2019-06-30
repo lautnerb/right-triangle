2aa4b7b781525ee57e6f6cc8ea079d3cd9a1debd
Balazs Lautner <lautner.balazs@gmail.com>
2019-06-30 18:44:42 +0200

    added test for calculation from a and c

--- test_right_triangle.py
+++ test_right_triangle.py
@@ -4,7 +4,13 @@ from right_triangle import RightTriangle
 
 
 class TestRightTriangle(unittest.TestCase):
-    def test_creation_from_leg_and_hypotenuse(self):
+    def test_creation_from_a_and_c(self):
+        triangle = RightTriangle(a=3, c=5)
+        self.assertAlmostEqual(triangle.a, 3)
+        self.assertAlmostEqual(triangle.a_angle, 36.86989765)
+        self.assertAlmostEqual(triangle.b_angle, 53.13010235)
+
+    def test_creation_from_b_and_c(self):
         triangle = RightTriangle(b=4, c=5)
         self.assertAlmostEqual(triangle.a, 3)
         self.assertAlmostEqual(triangle.a_angle, 36.86989765)

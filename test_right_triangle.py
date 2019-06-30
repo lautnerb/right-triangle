1c9e3a7f83983b22654e27dd654bf567da948a85
Balazs Lautner <lautner.balazs@gmail.com>
2019-06-30 18:12:42 +0200

    added test for angle calculation from a leg and hypotenuse

--- test_right_triangle.py
+++ test_right_triangle.py
@@ -7,6 +7,8 @@ class TestRightTriangle(unittest.TestCase):
     def test_creation_from_leg_and_hypotenuse(self):
         triangle = RightTriangle(b=4, c=5)
         self.assertAlmostEqual(triangle.a, 3)
+        self.assertAlmostEqual(triangle.a_angle, 36.86989765)
+        self.assertAlmostEqual(triangle.b_angle, 53.13010235)
 
 
 if __name__ == '__main__':

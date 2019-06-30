a4077f775ef704138370720e664d7525554942bf
Balazs Lautner <lautner.balazs@gmail.com>
2019-06-30 17:04:51 +0200

    added class RightTriangle

---
+++ right_triangle.py
@@ -0,0 +1,13 @@
+class RightTriangle:
+    def __init__(
+            self,
+            a: float = None,
+            b: float = None,
+            c: float = None,
+            a_angle: float = None,
+            b_angle: float = None):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._a_angle = a_angle
+        self._b_angle = b_angle

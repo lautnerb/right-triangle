4359f7a8ad05775a68f3e8e70ac71f1c461c68be
Balazs Lautner <lautner.balazs@gmail.com>
2020-08-09 13:25:15 +0200

    redesigned api

--- right_triangle.py
+++ right_triangle.py
@@ -4,169 +4,181 @@ import math
 class RightTriangle:
     def __init__(
             self,
-            a: float = None,
-            b: float = None,
-            c: float = None,
-            a_angle: float = None,
-            b_angle: float = None):
-        self._a = a
-        self._b = b
-        self._c = c
-        self._a_angle = a_angle
-        self._b_angle = b_angle
-        self._parameters = (a, b, c, a_angle, b_angle)
+            side_a: float,
+            side_b: float,
+            side_c: float,
+            angle_a: float,
+            angle_b: float,
+    ):
+        self._side_a = side_a
+        self._side_b = side_b
+        self._side_c = side_c
+        self._angle_a = angle_a
+        self._angle_b = angle_b
 
-        if self._a and self._b:
-            self._c = self._calculate_hypotenuse_from_legs(self._a, self._b)
-            self._raise_error_if_parameter_inconsistent(c, self._c)
-            self._calculate_angles()
-        elif self._a and self._c:
-            self._b = self._calculate_leg_from_other_leg_and_hypotenuse(
-                self._a, self._c)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._calculate_angles()
-        elif self._b and self._c:
-            self._a = self._calculate_leg_from_other_leg_and_hypotenuse(
-                self._b, self._c)
-            self._raise_error_if_parameter_inconsistent(a, self._a)
-            self._calculate_angles()
-        elif self._a and self._a_angle:
-            self._b = self._calculate_leg_from_other_leg_and_adjacent_angle(
-                self._a, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._c = self._calculate_hypotenuse_from_legs(self._a, self._b)
-            self._raise_error_if_parameter_inconsistent(c, self._c)
-            self._b_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b_angle, self._b_angle)
-        elif self._b and self._a_angle:
-            self._a = self._calculate_leg_from_other_leg_and_opposed_angle(
-                self._b, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._c = self._calculate_hypotenuse_from_legs(self._a, self._b)
-            self._raise_error_if_parameter_inconsistent(c, self._c)
-            self._b_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b_angle, self._b_angle)
-        elif self._c and self._a_angle:
-            self._a = self._calculate_leg_from_hypotenuse_and_opposed_angle(
-                self._c, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(a, self._a)
-            self._b = self._calculate_leg_from_other_leg_and_hypotenuse(
-                self._a, self._c)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._b_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b_angle, self._b_angle)
-        elif self._a and self._b_angle:
-            self._a_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._b_angle)
-            self._raise_error_if_parameter_inconsistent(a_angle, self._a_angle)
-            self._b = self._calculate_leg_from_other_leg_and_adjacent_angle(
-                self._a, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._c = self._calculate_hypotenuse_from_legs(self._a, self._b)
-            self._raise_error_if_parameter_inconsistent(c, self._c)
-        elif self._b and self._b_angle:
-            self._a_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._b_angle)
-            self._raise_error_if_parameter_inconsistent(a_angle, self._a_angle)
-            self._a = self._calculate_leg_from_other_leg_and_opposed_angle(
-                self._b, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
-            self._c = self._calculate_hypotenuse_from_legs(self._a, self._b)
-            self._raise_error_if_parameter_inconsistent(c, self._c)
-        elif self._c and self._b_angle:
-            self._a_angle = self._calculate_acute_angle_from_other_acute_angle(
-                self._b_angle)
-            self._raise_error_if_parameter_inconsistent(a_angle, self._a_angle)
-            self._a = self._calculate_leg_from_hypotenuse_and_opposed_angle(
-                self._c, self._a_angle)
-            self._raise_error_if_parameter_inconsistent(a, self._a)
-            self._b = self._calculate_leg_from_other_leg_and_hypotenuse(
-                self._a, self._c)
-            self._raise_error_if_parameter_inconsistent(b, self._b)
+    @property
+    def side_a(self) -> float:
+        return self._side_a
+
+    @property
+    def side_b(self) -> float:
+        return self._side_b
+
+    @property
+    def side_c(self) -> float:
+        return self._side_c
+
+    @property
+    def angle_a(self) -> float:
+        return self._angle_a
+
+    @property
+    def angle_b(self) -> float:
+        return self._angle_b
+
+    @property
+    def angle_c(self) -> float:
+        return 90
+
+    @classmethod
+    def make(
+            cls,
+            side_a: float = None,
+            side_b: float = None,
+            side_c: float = None,
+            angle_a: float = None,
+            angle_b: float = None,
+    ) -> "RightTriangle":
+        if side_a and side_b:
+            return cls.from_side_a_and_side_b(side_a, side_b)
+        elif side_a and side_c:
+            return cls.from_side_a_and_side_c(side_a, side_c)
+        elif side_b and side_c:
+            return cls.from_side_b_and_side_c(side_b, side_c)
+        elif side_a and angle_a:
+            return cls.from_side_a_and_angle_a(side_a, angle_a)
+        elif side_b and angle_a:
+            return cls.from_side_b_and_angle_a(side_b, angle_a)
+        elif side_c and angle_a:
+            return cls.from_side_c_and_angle_a(side_c, angle_a)
+        elif side_a and angle_b:
+            return cls.from_side_a_and_angle_b(side_a, angle_b)
+        elif side_b and angle_b:
+            return cls.from_side_b_and_angle_b(side_b, angle_b)
+        elif side_c and angle_b:
+            return cls.from_side_c_and_angle_b(side_c, angle_b)
         else:
-            raise ValueError(f"Insufficient parameters for a RightTriangle:\n"
-                             + self._parameters_to_string(*self._parameters))
+            raise ValueError(
+                f"Insufficient parameters for a RightTriangle: "
+                f"{side_a=}, {side_b=}, {side_c=}, {angle_a=}, {angle_b=}"
+            )
 
-    @property
-    def a(self) -> float:
-        return self._a
+    @classmethod
+    def from_side_a_and_side_b(cls, side_a: float, side_b: float) -> "RightTriangle":
+        side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+        angle_a, angle_b = calculate_acute_angles_from_legs(side_a, side_b)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @property
-    def b(self) -> float:
-        return self._b
+    @classmethod
+    def from_side_a_and_side_c(cls, side_a: float, side_c: float) -> "RightTriangle":
+        side_b = calculate_leg_from_other_leg_and_hypotenuse(side_a, side_c)
+        angle_a, angle_b = calculate_acute_angles_from_legs(side_a, side_b)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @property
-    def c(self) -> float:
-        return self._c
+    @classmethod
+    def from_side_b_and_side_c(cls, side_b: float, side_c: float) -> "RightTriangle":
+        side_a = calculate_leg_from_other_leg_and_hypotenuse(side_b, side_c)
+        angle_a, angle_b = calculate_acute_angles_from_legs(side_a, side_b)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @property
-    def a_angle(self) -> float:
-        return self._a_angle
+    @classmethod
+    def from_side_a_and_angle_a(cls, side_a: float, angle_a: float) -> "RightTriangle":
+        side_b = calculate_leg_from_other_leg_and_adjacent_angle(side_a, angle_a)
+        side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+        angle_b = calculate_acute_angle_from_other_acute_angle(angle_a)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @property
-    def b_angle(self) -> float:
-        return self._b_angle
+    @classmethod
+    def from_side_b_and_angle_a(cls, side_b: float, angle_a: float) -> "RightTriangle":
+        side_a = calculate_leg_from_other_leg_and_opposed_angle(side_b, angle_a)
+        side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+        angle_b = calculate_acute_angle_from_other_acute_angle(angle_a)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @staticmethod
-    def _calculate_leg_from_other_leg_and_hypotenuse(other_leg, hypotenuse):
-        leg = math.sqrt(hypotenuse ** 2 - other_leg ** 2)
-        return leg
+    @classmethod
+    def from_side_c_and_angle_a(cls, side_c: float, angle_a: float) -> "RightTriangle":
+        side_a = calculate_leg_from_hypotenuse_and_opposed_angle(side_c, angle_a)
+        side_b = calculate_leg_from_other_leg_and_hypotenuse(side_a, side_c)
+        angle_b = calculate_acute_angle_from_other_acute_angle(angle_a)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @staticmethod
-    def _calculate_hypotenuse_from_legs(leg_a, leg_b):
-        hypotenuse = math.sqrt(leg_a ** 2 + leg_b ** 2)
-        return hypotenuse
+    @classmethod
+    def from_side_a_and_angle_b(cls, side_a: float, angle_b: float) -> "RightTriangle":
+        angle_a = calculate_acute_angle_from_other_acute_angle(angle_b)
+        side_b = calculate_leg_from_other_leg_and_adjacent_angle(side_a, angle_a)
+        side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @staticmethod
-    def _calculate_leg_from_other_leg_and_adjacent_angle(
-            other_leg, adjacent_angle):
-        leg = other_leg / math.tan(math.radians(adjacent_angle))
-        return leg
+    @classmethod
+    def from_side_b_and_angle_b(cls, side_b: float, angle_b: float) -> "RightTriangle":
+        angle_a = calculate_acute_angle_from_other_acute_angle(angle_b)
+        side_a = calculate_leg_from_other_leg_and_opposed_angle(side_b, angle_a)
+        side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
-    @staticmethod
-    def _calculate_acute_angle_from_other_acute_angle(other_acute_angle):
-        angle = 90 - other_acute_angle
-        return angle
-
-    @staticmethod
-    def _calculate_leg_from_other_leg_and_opposed_angle(
-            other_leg, opposed_angle):
-        leg = other_leg * math.tan(math.radians(opposed_angle))
-        return leg
-
-    @staticmethod
-    def _calculate_leg_from_hypotenuse_and_opposed_angle(
-            hypotenuse, opposed_angle):
-        leg = hypotenuse * math.sin(math.radians(opposed_angle))
-        return leg
-
-    def _calculate_angles(self):
-        self._a_angle = math.degrees(math.asin(self.a / self.c))
-        self._b_angle = self._calculate_acute_angle_from_other_acute_angle(
-            self._a_angle)
-
-    def _raise_error_if_parameter_inconsistent(
-            self, parameter_value, expected_value):
-        if (parameter_value is not None
-                and parameter_value != expected_value):
-            raise ValueError(f"Inconsistent parameters for a RightTriangle:\n"
-                             + self._parameters_to_string(*self._parameters))
-
-    @staticmethod
-    def _parameters_to_string(a, b, c, a_angle, b_angle):
-        return (f"\ta = {a}\n"
-                + f"\tb = {b}\n"
-                + f"\tc = {c}\n"
-                + f"\ta_angle = {a_angle}\n"
-                + f"\tb_angle = {b_angle}")
+    @classmethod
+    def from_side_c_and_angle_b(cls, side_c: float, angle_b: float) -> "RightTriangle":
+        angle_a = calculate_acute_angle_from_other_acute_angle(angle_b)
+        side_a = calculate_leg_from_hypotenuse_and_opposed_angle(side_c, angle_a)
+        side_b = calculate_leg_from_other_leg_and_hypotenuse(side_a, side_c)
+        return cls(side_a, side_b, side_c, angle_a, angle_b)
 
     def __str__(self):
-        return (f"RightTriangle"
-                + f"\n\ta = {self.a}"
-                + f"\n\tb = {self.b}"
-                + f"\n\tc = {self.c}"
-                + f"\n\tA = {self.a_angle: .2f}°"
-                + f"\n\tB = {self.b_angle: .2f}°")
+        return (
+            "RightTriangle" + "\n"
+            "\t" + f"{self.side_a=}" + "\n"
+            "\t" + f"{self.side_b=}" + "\n"
+            "\t" + f"{self.side_c=}" + "\n"
+            "\t" + f"{self.angle_a=}" + "\n"
+            "\t" + f"{self.angle_b=}"
+        )
+
+
+def calculate_acute_angles_from_legs(side_a: float, side_b: float):
+    side_c = calculate_hypotenuse_from_legs(side_a, side_b)
+    angle_a = math.degrees(math.asin(side_a / side_c))
+    angle_b = calculate_acute_angle_from_other_acute_angle(angle_a)
+    return angle_a, angle_b
+
+
+def calculate_leg_from_hypotenuse_and_opposed_angle(
+        hypotenuse, opposed_angle):
+    leg = hypotenuse * math.sin(math.radians(opposed_angle))
+    return leg
+
+
+def calculate_leg_from_other_leg_and_opposed_angle(
+        other_leg, opposed_angle):
+    leg = other_leg * math.tan(math.radians(opposed_angle))
+    return leg
+
+
+def calculate_acute_angle_from_other_acute_angle(other_acute_angle):
+    angle = 90 - other_acute_angle
+    return angle
+
+
+def calculate_leg_from_other_leg_and_adjacent_angle(
+        other_leg, adjacent_angle):
+    leg = other_leg / math.tan(math.radians(adjacent_angle))
+    return leg
+
+
+def calculate_hypotenuse_from_legs(leg_a, leg_b):
+    hypotenuse = math.sqrt(leg_a ** 2 + leg_b ** 2)
+    return hypotenuse
+
+
+def calculate_leg_from_other_leg_and_hypotenuse(other_leg, hypotenuse):
+    leg = math.sqrt(hypotenuse ** 2 - other_leg ** 2)
+    return leg

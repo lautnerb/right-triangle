9374c1e1a9ec6a857f56d6a7b307c57c576ac692
Balazs Lautner <lautner.balazs@gmail.com>
2020-08-30 11:40:44 +0200

    added docstrings

--- right_triangle.py
+++ right_triangle.py
@@ -1,7 +1,36 @@
+"""Calculations with right-angled (or simply right) triangles.
+
+The terminology of right triangles:
+
+A right triangle has one right angle (90-degree angle).
+
+The other two angles are called acute angles (less than 90-degree angles).
+
+The side opposite the right angle is called the hypotenuse.
+
+The sides adjacent to the right angle are called legs.
+"""
+
 import math
 
 
 class RightTriangle:
+    """Representation of a right-angled (or right) triangle.
+
+    A right triangle is a triangle in which one angle is a right angle
+    (90-degree angle) (angle C).
+
+    The side opposite the right angle is called the hypotenuse (side c).
+
+    The sides adjacent to the right angle are called legs (side a and side b).
+
+    Side a is the side adjacent to angle B and opposed to angle A.
+
+    Side b is the side adjacent to angle A and opposed to angle B.
+
+    Do not use the default constructor.
+    Use one of the factory methods instead (make, from_*).
+    """
     def __init__(
             self,
             side_a: float,
@@ -10,6 +39,10 @@ class RightTriangle:
             angle_a: float,
             angle_b: float,
     ):
+        """Do not use the default constructor.
+
+        Use one of the factory methods instead (make, from_*).
+        """
         self._side_a = side_a
         self._side_b = side_b
         self._side_c = side_c
@@ -18,26 +51,32 @@ class RightTriangle:
 
     @property
     def side_a(self) -> float:
+        """The length of side a."""
         return self._side_a
 
     @property
     def side_b(self) -> float:
+        """The length of side b."""
         return self._side_b
 
     @property
     def side_c(self) -> float:
+        """The length of side c (the hypotenuse)."""
         return self._side_c
 
     @property
     def angle_a(self) -> float:
+        """Angle A in degrees."""
         return self._angle_a
 
     @property
     def angle_b(self) -> float:
+        """Angle B in degrees."""
         return self._angle_b
 
     @property
     def angle_c(self) -> float:
+        """Angle C in degrees. It is the right angle, so it is always 90."""
         return 90
 
     @classmethod
@@ -49,6 +88,11 @@ class RightTriangle:
             angle_a: float = None,
             angle_b: float = None,
     ) -> "RightTriangle":
+        """Universal constructor for ``RightTriangle``.
+
+        Provide the length of one side and any other attribute,
+        and it constructs the ``RightTriangle`` object.
+        """
         if side_a and side_b:
             return cls.from_side_a_and_side_b(side_a, side_b)
         elif side_a and side_c:
